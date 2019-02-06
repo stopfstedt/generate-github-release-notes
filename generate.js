076a657e48eb8e4c7e84d4a916a0e57260d33192
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-06 08:30:42 -0800

    Update to latest github package and API
    Github changed their client to octokit/rest and the API has changed
    quite a bit so I updated our calls.

--- generate.js
+++ generate.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const meow = require('meow');
-const GitHubApi = require('github');
+const Octokit = require('@octokit/rest');
 const generateReleaseNotes = require('./lib/generateReleaseNotes');
 
 const cli = meow(`
@@ -25,25 +25,20 @@ const repository = cli.input[1];
 const sinceTag = cli.input[2];
 const releaseTag = cli.input[3];
 
-const github = new GitHubApi({
-  debug: false,
-  protocol: 'https',
-  pathPrefix: '',
-  headers: {
-    'user-agent': 'github release notes'
-  },
-  promise: Promise,
-  timeout: 5000
-});
-
+let auth = undefined;
 if (cli.flags.githubApiToken) {
-  github.authenticate({
-    type: 'token',
-    token: cli.flags.githubApiToken,
-  });
+  auth = cli.flags.githubApiToken;
 }
 
-generateReleaseNotes(github, owner, repository, sinceTag, releaseTag).then(releaseNotes => {
+const octokit = new Octokit({
+  auth,
+  userAgent: 'github release notes',
+  request: {
+    timeout: 5000
+  }
+});
+
+generateReleaseNotes(octokit, owner, repository, sinceTag, releaseTag).then(releaseNotes => {
   console.log(releaseNotes);
   process.exit(0);
 });
