0a68b0b87e5945292c52236ce4fe0f50fef3d63f
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-01-03 16:13:16 -0800

    Add a real world test
    Not the most isolated tests in the world, but it ensures everything is
    actually working.

---
+++ test/test.js
@@ -0,0 +1,35 @@
+const Octokit = require('@octokit/rest');
+const generateReleaseNotes = require('../lib/generateReleaseNotes');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+let auth = undefined;
+if (process.env.GITHUB_API_TOKEN) {
+  auth = process.env.GITHUB_API_TOKEN;
+}
+
+const octokit = new Octokit({
+  auth,
+  userAgent: 'github release notes',
+  request: {
+    timeout: 5000
+  }
+});
+
+const samplePath = path.join(__dirname, 'SampleNotes.md');
+const sampleContent = fs.readFileSync(samplePath, 'utf-8', 'r+');
+
+describe('Real Notes', function() {
+  it('generates notes for this repo', async function () {
+    const notes = await generateReleaseNotes(
+      octokit,
+      'jrjohnson',
+      'generate-github-release-notes',
+      'v1.2.1',
+      'v1.3.0'
+    );
+    assert.ok(notes.length > 0);
+    assert.equal(notes, sampleContent);
+  });
+});
